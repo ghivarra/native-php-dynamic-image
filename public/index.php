0ef836784206385f22d212d64a8e860dede7f090
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-05-14 12:26:10 +0700

    done creating public/index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
 <?php
+
+// Check PHP versions
+$acceptedPHPVersion = '8.3';
+
+if (PHP_VERSION < $acceptedPHPVersion)
+{
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo "Your PHP Version must be at least {$acceptedPHPVersion} or higher.";
+    exit();
+}
+
+// Set path constants
+define('PUBLICPATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('ROOTPATH', realpath(PUBLICPATH . '..' . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR);
+define('APPPATH', realpath(ROOTPATH . 'app') . DIRECTORY_SEPARATOR);
+
+// load ../app/Boot.php
+require_once APPPATH . 'Boot.php';
+
+// away we go...
+exit(App\Boot::run());
